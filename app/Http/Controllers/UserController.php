53f19430d72972629da81dca9abe31ffc96627b8
fharavena <fharavena@gmail.com>
2019-09-02 00:40:31 -0400

    rutas

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,20 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
 
-    public function pruebas(Request $request){
+    public function pruebas(Request $request)
+    {
         return "Accion de pruebas de User-Controller";
+    }
+
+    public function register(Request $request)
+    {
+        $name = $request->input('name');
+        $surname = $request->input('surname');
+        return "Accion de registro de usuarios: $name $surname";
+    }
+
+    public function login(Request $request)
+    {
+        return "Accion de login de usuarios";
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,18 +21,32 @@ Route::get('/welcome', function () {
 });
 
 
-Route::get('/pruebas/{nombre?}', function($nombre=null){
+Route::get('/pruebas/{nombre?}', function ($nombre = null) {
     $texto =  '<h2>Text desde una ruta</h2>';
-    $texto .= 'Nombre: '.$nombre;
+    $texto .= 'Nombre: ' . $nombre;
     return view('pruebas', array(
-        'texto'=> $texto
+        'texto' => $texto
     ));
 });
 
 Route::get('/animales', 'PruebasController@index');
 Route::get('/test-orm', 'PruebasController@testOrm');
 
+/*
+Metodos HTTP
+
+*GET: Conseguir datos o recuross
+*POST: Guardar datos o recursos o hacer logica desde un formulario
+*PUT: Actualizar datos o recursos
+*DELETE: Eliminar datos o recursos
+*/
+
 //Rutas de api
 Route::get('/usuario/pruebas', 'UserController@pruebas');
 Route::get('/categoria/pruebas', 'CategoryController@pruebas');
 Route::get('/post/pruebas', 'PostController@pruebas');
+
+
+//Rutas del controlador de usuario
+Route::post('/api/register', 'UserController@register');
+Route::post('/api/login', 'UserController@login');
